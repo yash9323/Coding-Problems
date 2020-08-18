b05077533eb8e55f331c00f2cb17331aa2c00d63
yash9323 <50073863+yash9323@users.noreply.github.com>
2020-08-18 16:01:56 +0530

    Add files via upload

---
+++ prob.py
@@ -0,0 +1,21 @@
+import sys
+import math
+def primefactors(n):
+    l=[]
+    while n%2==0:
+        l.append(2)
+        n=n/2
+    
+    for i in range(3,int(math.sqrt(n)+1),2):
+        while n%i==0:
+            l.append(int(i))
+            n=n/i
+    if n>2:
+        l.append(int(n))
+
+    return max(l)
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(primefactors(n))
