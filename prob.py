fa4abb4ae5580a612fec805f56855319cc05382a
yash9323 <50073863+yash9323@users.noreply.github.com>
2020-08-18 15:31:40 +0530

    Add files via upload

---
+++ prob.py
@@ -0,0 +1,23 @@
+import sys
+def even_fibo(n):
+    l=[1,1]
+    i=1
+    sume=0
+    while( l[i] <= n ) :
+        ab=l[i]+l[i-1]
+        l.append(ab)
+        i=i+1
+    l.pop()
+    for i in l:
+        if(i%2==0):
+            sume=sume+i
+    return sume
+
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    ans =even_fibo(n)
+    print(ans)
+    
+
