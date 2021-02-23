5c6a8bc1ba762429718ff79161aa2f363ab74adb
Yash Bhutoria <50073863+yash9323@users.noreply.github.com>
2021-02-23 12:21:50 +0530

    Create problem9.py

---
+++ Project Euler/problem9.py
@@ -0,0 +1,19 @@
+"""
+Project Euler Problem 9
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+
+"""
+for a in range(150,400):
+    for b in range(a+1,600):
+        c=(a**2+b**2)**0.5
+        if a+b+c==1000:
+            print(a,b,c)
+            print("The product of a*b*c is :",int(a*b*c))
+
+
